cdc27c4dcb1de71cb043db2cae0c40df62204e52
vanecordero <ing.soft.cordero@gmail.com>
2021-10-07 23:29:23 -0400

    2nd commit

---
+++ src/pages/menu.js
@@ -0,0 +1,25 @@
+import planetsData from '../services/PlanetsData';
+import React, { useState, useEffect } from 'react';
+
+export default function Menu(){
+    // const ELEM = 'Mercury'
+   const [planetas, setPlanetData] = useState([])
+  
+   useEffect(function(){
+      setPlanetData(planetsData())
+   },[])
+ 
+   return (
+     <>
+       {
+         planetas.map(planeta=>{
+             console.log(planeta)
+         // return <a href="#">{planeta['name']}</a>
+         })
+       }
+     </>
+   )
+
+
+
+   }
